1740b15abf49952c3e1a941c7822fa68920f01d4
dpopritkin <dpopritkin@certant.com>
2020-03-30 14:50:46 -0300

    4 files modified, 3 added

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {CartService} from '../cart.service'
+import {CartService} from '../cart.service';
+import { FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-cart',
@@ -8,12 +10,27 @@ import {CartService} from '../cart.service'
 })
 export class CartComponent implements OnInit {
 items;
+checkoutForm;
   constructor(
-    private cart :CartService,
-  ) { }
+    private cartService :CartService,
+    private formBuilder: FormBuilder,
+  ) {
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+   }
 
   ngOnInit() {
-    this.items = this.cart.getItems();
+    this.items = this.cartService.getItems();
+  }
+
+  onSubmit(customerData) {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+
+    console.warn('Your order has been submitted', customerData);
   }
 
 }
